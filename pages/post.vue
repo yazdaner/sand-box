a40a98ed9df975bf74870f34fa1236b84dfbdf89
yazdan <yazdaner8@gmail.com>
2024-04-27 11:26:30 +0330

    fetch

--- pages/post.vue
+++ pages/post.vue
@@ -1,7 +1,20 @@
 <template>
     <div>
-        <LayoutsHeader />
-        <h1>Post index</h1>
-        <NuxtPage />
+        <div v-for="post in posts" :key="post.id">
+            <h3>{{ post.id }} - {{ post.title }}</h3>
+            <p>{{ post.body }}</p>
+        </div>
     </div>
 </template>
+<script setup>
+const posts = ref([]);
+try {
+    const data = await $fetch("posts", {
+        baseURL: 'https://jsonplaceholder.typicode.com/',
+        query: { _limit : 5 }
+    });
+    posts.value = data;
+} catch (error) {
+    console.log(error);
+}
+</script>
