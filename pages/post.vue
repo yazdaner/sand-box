478037fcfe3470d4c7260c2f8e071a0438cc224a
yazdan <yazdaner8@gmail.com>
2024-04-27 12:14:08 +0330

    useFetch

--- pages/post.vue
+++ pages/post.vue
@@ -5,16 +5,24 @@
             <p>{{ post.body }}</p>
         </div>
     </div>
+    <button @click="next">next</button>
+    <button @click="previous">previous</button>
+    <div v-if="pending">loading ...</div>
 </template>
 <script setup>
-const posts = ref([]);
-try {
-    const data = await $fetch("posts", {
-        baseURL: 'https://jsonplaceholder.typicode.com/',
-        query: { _limit : 5 }
-    });
-    posts.value = data;
-} catch (error) {
-    console.log(error);
+
+const start = ref(0);
+
+// const { data : posts, pending, error, refresh } = useLazyAsyncData('posts', () => $fetch(`https://jsonplaceholder.typicode.com/posts?_start=${start.value}&_limit=5`));
+const { data : posts, pending, error, refresh } =  useLazyFetch(() => `https://jsonplaceholder.typicode.com/posts?_start=${start.value}&_limit=5`);
+
+function next(){
+    start.value += 5;
+    refresh();
+}
+
+function previous(){
+    start.value -= 5;
+    refresh();
 }
 </script>
